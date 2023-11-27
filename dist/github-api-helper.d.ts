6147d3b2058308e1bd1380f0b78fd1a011b2634a
moki <morozov.kirill.moki@gmail.com>
2023-11-27 19:27:57 +0300

    feat!: manual trigger action

--- dist/github-api-helper.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function downloadRepository(authToken: string, owner: string, repo: string, ref: string, commit: string, repositoryPath: string, baseUrl?: string): Promise<void>;
-/**
- * Looks up the default branch name
- */
-export declare function getDefaultBranch(authToken: string, owner: string, repo: string, baseUrl?: string): Promise<string>;
