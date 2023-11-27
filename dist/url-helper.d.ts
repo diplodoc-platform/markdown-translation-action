d3ec51c946b61eff4758de07189e0018ffdaf7f5
moki <morozov.kirill.moki@gmail.com>
2023-11-27 19:27:57 +0300

    feat!: manual trigger action

--- dist/url-helper.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { URL } from 'url';
-import { IGitSourceSettings } from './git-source-settings';
-export declare function getFetchUrl(settings: IGitSourceSettings): string;
-export declare function getServerUrl(url?: string): URL;
-export declare function getServerApiUrl(url?: string): string;
-export declare function isGhes(url?: string): boolean;
--- dist/workflow-context-helper.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Gets the organization ID of the running workflow or undefined if the value cannot be loaded from the GITHUB_EVENT_PATH
- */
-export declare function getOrganizationId(): Promise<number | undefined>;
